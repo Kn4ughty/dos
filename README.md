597d8067463ac567a8240e74c46e69f2f5acbed2
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2026-04-05 10:00:57 +1000

    add improved error codes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
 resources:
-https://wiki.osdev.org/Babystep3
-
-http://www.baldwin.cx/386htm/c17.htm
+- https://wiki.osdev.org/Babystep3
+- http://www.baldwin.cx/386htm/c17.htm
+- https://www.ctyme.com/rbrown.htm
--- boot.asm
+++ boot.asm
@@ -7,6 +7,8 @@ jmp start
     %include "print.asm"
 
 start:
+    mov [boot_drive], dl ; save drive id
+
 	xor ax, ax
 	mov ds, ax
     mov es, ax
@@ -16,10 +18,10 @@ start:
 	cli
 
 
+    ; https://www.ctyme.com/intr/rb-0706.htm
     mov ah, 0x41
     mov bx, 0x55AA
-
-    mov dl, 0x80
+    mov dl, [boot_drive]
     int 0x13
 
     cmp bx, 0xAA55
@@ -31,13 +33,14 @@ start:
     mov si, RW_FAIL
     jnz ERROR
 
+    ; https://www.ctyme.com/intr/rb-0708.htm
     mov si, DAPACK
     mov ah, 0x42
-    mov dl, 0x80
+    mov dl, [boot_drive]
     int 0x13
-    mov si, MYSTERY
-    ; jmp ERROR
-    ; jc short ERROR
+    mov si, READ_ERROR
+    ; jump carry
+    jc short ERROR
 
 
     mov al, [blkcnt]
@@ -52,7 +55,12 @@ start:
 
 ; prints string at adress in si
 ERROR:
+    call println
+    mov si, EC_PREAMBLE
     call sprint
+    ; print error code: https://www.ctyme.com/intr/rb-0606.htm#Table234
+    mov [reg16], ah
+    call printreg16
     jmp $
 
 
@@ -60,13 +68,11 @@ gdtinfo:
    dw gdt_end - gdt - 1   ;last byte in table
    dd gdt         ;start of table
 
-    ; mov bl, al
-port60   dw 0
-
+boot_drive db 0
 DAPACK:
     db 0x10
     db 0
-blkcnt: dw 16 ; read 16 blocks
+blkcnt: dw 5 ; read n blocks
 db_add: 
     dw NEXT_SECTOR; memory destination
     dw 0 ; memory page
@@ -74,9 +80,10 @@ d_lba:
     dd 1 ; put the lba to read in this spot
     dd 0 ; more storage bytes only for big lba
 
-BX_SWAP_FAIL db `bx not swapped!\n`, 0
-RW_FAIL db `read_write not supported in cx \n`, 0
-MYSTERY db `mystery\n`, 0
+BX_SWAP_FAIL db `bx not swapped!`, 0
+RW_FAIL db `read_write not supported in cx`, 0
+READ_ERROR db `Error reading drive`, 0
+EC_PREAMBLE db `Error code: 0x`, 0
 
 gdt dd 0,0
 flatdesc db 0xff, 0xff, 0,0,0, 0b10010010, 0b11001111, 0
@@ -86,4 +93,9 @@ times 510-($-$$) db 0
 db 0x55
 db 0xAA
 NEXT_SECTOR:
-next_sector_txt db "hello look at this!", 0
+next_sector_txt db "Successfully loaded next sector", 0
+
+next_code:
+
+
+times (512*5) db 0
