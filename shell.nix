bd5c23ffbcbcebd4a2c5365c8b0e8ef6d9eedf20
Kn4ughty <77908661+Kn4ughty@users.noreply.github.com>
2026-04-14 08:53:43 +1000

    nixify!

---
+++ shell.nix
@@ -0,0 +1,21 @@
+# { pkgs ? import <nixpkgs> {} }:
+# pkgs.mkShell {
+#   buildInputs =  with pkgs; [
+#   cargo-bootimage
+#   ];
+# }
+
+{ pkgs ? import <nixpkgs> {} }:
+let
+  rust-overlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  pkgs = import <nixpkgs> {
+    overlays = [(import rust-overlay)];
+  };
+  toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+in
+  pkgs.mkShell {
+    packages = with pkgs; [
+      toolchain
+      cargo-bootimage
+    ];
+}
