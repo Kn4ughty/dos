c64e951ca80c13b3c6d3c841f164ce7124a05482
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2026-04-13 22:42:22 +1000

    basic test infra

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 mod volatile;
 
+use core::arch::asm;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -12,9 +16,42 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Failed);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    unsafe {
+        asm!("out dx, eax", in("eax") exit_code as u32, in("dx") 0xf4u16);
+    }
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello world!");
+
+    #[cfg(test)]
+    test_main();
 
     #[allow(clippy::empty_loop)]
     loop {}
