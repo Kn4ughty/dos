8f066659bc6751d45a363a7cfdbb98a1195cba41
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2026-04-19 10:53:40 +1000

    Get interrupts from PIC

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,7 +3,30 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use lazy_static::lazy_static;
 
 use crate::gdt;
-use crate::vga_println;
+use crate::pic::ChainedPics;
+use crate::{vga_print, vga_println};
+
+use spin;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+// Safety. The offsets need to be correct
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+    // why lol
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+}
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -15,6 +38,8 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        use core::ops::IndexMut;
+        idt.index_mut(InterruptIndex::Timer.as_u8()).set_handler_fn(timer_interrupt_handler);
         idt
     };
 }
@@ -29,6 +54,15 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
 #[test_case]
 fn test_breakpoint_exception() {
     x86_64::instructions::interrupts::int3();
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    vga_print!(".");
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8())
+    }
 }
 
 extern "x86-interrupt" fn double_fault_handler(
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use core::panic::PanicInfo;
 
 pub mod gdt;
 pub mod interrupts;
+pub mod pic;
 pub mod port;
 pub mod serial;
 pub mod vga_buffer;
@@ -89,6 +90,8 @@ fn panic(info: &PanicInfo) -> ! {
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 #[cfg(test)]
