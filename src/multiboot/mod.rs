5a18e1b4548fd603710472a2af45274a970aaee3
Kn4ughty <77908661+Kn4ughty@users.noreply.github.com>
2026-05-07 21:47:20 +1000

    Improve comments

--- src/multiboot/mod.rs
+++ src/multiboot/mod.rs
@@ -14,17 +14,26 @@ pub struct BootInformationFormat {
 }
 
 impl BootInformationFormat {
-    /// Safety
-    /// Must be valid address to BootInformationFormat
+    /// SAFETY
+    /// Caller must pass in valid address to the multiboot2 header.
     pub unsafe fn load<'a>(addr: usize) -> &'a BootInformationFormat {
+        assert_eq!(addr % 8, 0, "Multiboot Header must be 8-byte aligned");
+
         let multiboot = unsafe { &*(addr as *const BootInformationFormat) };
-        assert_eq!(multiboot.total_size % 8, 0);
+
+        assert_eq!(
+            multiboot.total_size % 8,
+            0,
+            "Total size must be 8b aligned. (every tag is 8 byte alligned, so it follows the size must be too)"
+        );
+
         multiboot
     }
 
     pub fn get<T: TagType>(&self) -> Result<&T, TagError> {
         self.tags()
             .find(|t| t.typ == T::ID)
+            // SAFETY. Should be the correct type, as the ID matches for T
             .map(|tag| unsafe { &*(tag as *const TagHeader as *const T) })
             .ok_or(TagError::NotFound)
             .and_then(|tag| tag.validate())
@@ -32,9 +41,9 @@ impl BootInformationFormat {
 
     fn tags(&self) -> TagIter {
         let t = TagIter {
+            total_size: self.total_size as usize,
             current: &self.first_tag as *const _,
         };
-        assert_eq!(unsafe { *t.current }, self.first_tag);
         t
     }
 }
@@ -55,6 +64,7 @@ struct TagHeader {
 
 #[derive(Debug)]
 struct TagIter {
+    total_size: usize,
     current: *const TagHeader,
 }
 
@@ -65,15 +75,28 @@ impl Iterator for TagIter {
         // SAFETY. Since previous iteration should have set as valid tag its okay
         let tag = unsafe { &*self.current };
 
-        assert!(tag.size >= 8);
+        assert!(
+            tag.size >= 8,
+            "Tags cannot be less than 8 bytes in size, since that would be smaller than the header"
+        );
 
         if tag.typ == 0 && tag.size == 8 {
             return None;
         }
 
         let mut next_addr = self.current as usize;
+
+        // Align with padding to next 8 bytes
         next_addr += ((tag.size + 7) & !0x7) as usize;
-        assert_eq!(next_addr % 8, 0);
+        // This basically just checks that the padding math is correct, so its just for debug mode.
+        debug_assert_eq!(next_addr % 8, 0, "Tags must be 8 byte aligned.");
+
+        // This assert _should_ only trigger if the end tag is missing, or a tag is marked with an
+        // incorrect (and large) size.
+        assert!(
+            self.total_size < next_addr,
+            "Cannot exceed total size of multiboot structure"
+        );
 
         let result = tag;
         self.current = next_addr as *const TagHeader;
